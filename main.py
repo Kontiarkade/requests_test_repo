4bc9925a4d55d4a43e07d97821ea640e331227f7
kontiarkade <kontiarkade@gmail.com>
2020-04-14 05:29:07 -0500

    work work got config done

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import click
 import re
-
 import requests
 import socket
-
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import getpass
+import time
+import yaml
+
+# Default values:
+url_string = 'https://nordvpn.com/ru/ovpn/'
+regex = re.compile(r'<span class="mr-2">'
+                   r'(\S*)'
+                   r'</span>')
+
 
 def isOpen(host, port=80):
     '''
@@ -14,7 +23,7 @@ def isOpen(host, port=80):
     host - string
     port - string / int
     
-    Returns string, bool
+    Returns (string, bool)
     '''
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -31,8 +40,16 @@ def getContent(url_string):
     
     Returns string
     '''
-    r = requests.get(url_string)
-    return r.text
+    try:
+        print('Geting content from ', url_string)
+        r = requests.get(url_string)
+        return r.text
+    except requests.exceptions.ConnectionError as e:
+        raise SystemExit(str(e).strip('()'))
+    except requests.exceptions.SSLError as e:
+        raise SystemExit(str(e).strip('()'))
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(str(e).strip('()'))
 
 def findMatches(obj, regex):
     '''
@@ -42,6 +59,7 @@ def findMatches(obj, regex):
     
     Returns list
     '''
+    print('Finding servers...')
     match = regex.findall(obj)
     if match: return match
 
@@ -58,42 +76,76 @@ def readFile(f):
         while True:
             a = f.read(1024)
             if not a: break
-            b = b + a
+            b += a
         return b
 
+def writeFile(d):
+    print('Making yaml with all servers...')
+    timestr = 'all_servers_' + time.strftime("%Y%m%d-%H%M%S") + '.yml'
+    with open (timestr, 'w') as f:
+        yaml.dump(d, f)
+        
 def generateProxy(hosts):
+    '''
+    Generating proxys list
+    hosts - list of strings
+    
+    Returns dictionary
+    '''
+    print('Generating proxys list...')
     with ThreadPoolExecutor(max_workers=10) as executor:
         future_list = []
-        t = []
+        tlist = []
+        flist = []
         for host in hosts:
             future = executor.submit(isOpen, host, '80')
             future_list.append(future)
-        for f in as_completed(future_list):
-            t.append(f.result())
-        return t
-
+        with click.progressbar(length=len(future_list),
+                               show_pos=True,
+                               fill_char='#',
+                               empty_char=' ') as bar:           
+            for f in as_completed(future_list):
+                if f.result()[1]:
+                    tlist.append(f.result()[0])
+                else:
+                    flist.append(f.result()[0])                
+                bar.update(1)
+        d = {True:tlist, False:flist}
+        return d
   
+def generateConfig(login, password, proxy_d, port=80):
+    print('Making config file...')
+    timestr = 'config_file_' + time.strftime("%Y%m%d-%H%M%S") + '.txt'
+    with open (timestr, 'w') as f:
+        for host in proxy_d[True]:
+            s = 'http://' + login + ':' + password + '@' + host + ':' + str(port) + '\r\n'
+            f.write(s)            
 
-
-if __name__ == '__main__':
-    
-    thelist = ['us4948.nordvpn.com', 'us4949.nordvpn.com', 'us4950.nordvpn.com', 'us4951.nordvpn.com', 'us4952.nordvpn.com', 'us4953.nordvpn.com', 'us4955.nordvpn.com', 'us5000.nordvpn.com', 'us5001.nordvpn.com', 'us5002.nordvpn.com', 'us5003.nordvpn.com', 'us5004.nordvpn.com', 'us5005.nordvpn.com', 'us5006.nordvpn.com', 'us5007.nordvpn.com', 'us5008.nordvpn.com', 'us5009.nordvpn.com', 'us5010.nordvpn.com', 'us5011.nordvpn.com', 'us5012.nordvpn.com', 'us5013.nordvpn.com', 'us5014.nordvpn.com', 'us5015.nordvpn.com', 'us5016.nordvpn.com', 'us5017.nordvpn.com', 'us5019.nordvpn.com', 'us502.nordvpn.com', 'us5020.nordvpn.com', 'us5021.nordvpn.com', 'us5022.nordvpn.com']
-    
-    tl = ['us4948.nordvpn.com', 'us4949.nordvpn.com', 'us4950.nordvpn.com']
-    
-    
-    regex = re.compile(r'<span class="mr-2">'
-           r'(\S*)'
-           r'</span>')
+if __name__ == '__main__':  
     '''
-    t = getContent('https://nordvpn.com/ru/ovpn/')
+    thelist = ['us4948.nordvpn.com', 'us4949.nordvpn.com', 'us4950.nordvpn.com', 'us4951.nordvpn.com', 'us4952.nordvpn.com', 'us4953.nordvpn.com', 'us4955.nordvpn.com', 'us5000.nordvpn.com', 'us5001.nordvpn.com', 'us5002.nordvpn.com', 'us5003.nordvpn.com', 'us5004.nordvpn.com', 'us5005.nordvpn.com', 'us5006.nordvpn.com', 'us5007.nordvpn.com', 'us5008.nordvpn.com', 'us5009.nordvpn.com', 'us5010.nordvpn.com', 'us5011.nordvpn.com', 'us5012.nordvpn.com', 'us5013.nordvpn.com', 'us5014.nordvpn.com', 'us5015.nordvpn.com', 'us5016.nordvpn.com', 'us5017.nordvpn.com', 'us5019.nordvpn.com', 'us502.nordvpn.com', 'us5020.nordvpn.com', 'us5021.nordvpn.com', 'us5022.nordvpn.com']
+    tl = ['us4948.nordvpn.com', 'us4949.nordvpn.com', 'us4950.nordvpn.com']    
+    d = {True: ['us4949.nordvpn.com', 'us4948.nordvpn.com', 'us5001.nordvpn.com', 'us5000.nordvpn.com', 'us5002.nordvpn.com', 'us5007.nordvpn.com', 'us5006.nordvpn.com', 'us5004.nordvpn.com', 'us5003.nordvpn.com', 'us5005.nordvpn.com', 'us5009.nordvpn.com', 'us5008.nordvpn.com', 'us5011.nordvpn.com', 'us5012.nordvpn.com', 'us5010.nordvpn.com', 'us5013.nordvpn.com', 'us5015.nordvpn.com', 'us5017.nordvpn.com', 'us5016.nordvpn.com', 'us5019.nordvpn.com', 'us5021.nordvpn.com', 'us502.nordvpn.com', 'us5020.nordvpn.com', 'us5022.nordvpn.com', 'us5014.nordvpn.com'], False: ['us4951.nordvpn.com', 'us4950.nordvpn.com', 'us4952.nordvpn.com', 'us4955.nordvpn.com', 'us4953.nordvpn.com']}
+
+    getContent(url_string)
     l = findMatches(t, regex)
     with open ('all_servers.txt', 'w') as f:
         for line in l:
             f.write(line + '\r\n')    
-    '''    
-    t = getContent('https://nordvpn.com/ru/ovpn/')
-    l = findMatches(t, regex)
-    print(generateProxy(l))
-    
-    
+    #Http://login:pass@server:port
+    '''
+    t = getContent(url_string)
+    try:
+        l = findMatches(t, regex)    
+        d = generateProxy(thelist)
+        writeFile(d)
+        print('Proxys list successfully generated!')
+    except:
+        raise SystemExit('Something went terrible wrong!')
+    try:
+        login = input('Input login: ')
+        password = getpass.getpass()
+        generateConfig(login, password, d)
+    except:
+        raise SystemExit('Something went terrible wrong!')
+    print('Success!')
