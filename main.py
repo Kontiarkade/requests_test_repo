f61457741c49978be5bd24eb4cdd6a3637d5a354
kontiarkade <kontiarkade@gmail.com>
2020-04-14 01:59:27 -0500

    work work

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 
 import re
 
+import requests
 import socket
-import ssl
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-def isOpen(host, port):
+def isOpen(host, port=80):
     '''
     Checking host for open port
     host - string
@@ -31,39 +31,8 @@ def getContent(url_string):
     
     Returns string
     '''
-    result = ''
-    s = socket.socket()
-    host = socket.gethostbyname(url_string)
-    port = 80
-    s.connect((host,port))
-    s.sendall(b"GET /\r\n")
-    while True:
-        data = s.recv(1024)
-        if not data: break
-        result += data.decode('utf-8')
-    return result
-
-def getSSLContent(url_string):
-    '''
-    Loading web page form provided url by using https
-    url_string - string
-    
-    Returns string
-    '''
-    result = ''
-    s = socket.socket()
-    host = socket.gethostbyname(url_string)
-    port = 443
-    s.connect((host,port))
-    s = ssl.wrap_socket(s, keyfile=None, certfile=None,
-                    server_side=False, cert_reqs=ssl.CERT_NONE,
-                    ssl_version=ssl.PROTOCOL_SSLv23)
-    s.sendall(b"GET /\r\n")
-    while True:
-        data = s.recv(1024)
-        if not data: break
-        result += data.decode('utf-8')
-    return result
+    r = requests.get(url_string)
+    return r.text
 
 def findMatches(obj, regex):
     '''
@@ -103,18 +72,28 @@ def generateProxy(hosts):
             t.append(f.result())
         return t
 
+  
+
+
 if __name__ == '__main__':
-    #print(getSSLContent('www.google.com'))
+    
+    thelist = ['us4948.nordvpn.com', 'us4949.nordvpn.com', 'us4950.nordvpn.com', 'us4951.nordvpn.com', 'us4952.nordvpn.com', 'us4953.nordvpn.com', 'us4955.nordvpn.com', 'us5000.nordvpn.com', 'us5001.nordvpn.com', 'us5002.nordvpn.com', 'us5003.nordvpn.com', 'us5004.nordvpn.com', 'us5005.nordvpn.com', 'us5006.nordvpn.com', 'us5007.nordvpn.com', 'us5008.nordvpn.com', 'us5009.nordvpn.com', 'us5010.nordvpn.com', 'us5011.nordvpn.com', 'us5012.nordvpn.com', 'us5013.nordvpn.com', 'us5014.nordvpn.com', 'us5015.nordvpn.com', 'us5016.nordvpn.com', 'us5017.nordvpn.com', 'us5019.nordvpn.com', 'us502.nordvpn.com', 'us5020.nordvpn.com', 'us5021.nordvpn.com', 'us5022.nordvpn.com']
+    
+    tl = ['us4948.nordvpn.com', 'us4949.nordvpn.com', 'us4950.nordvpn.com']
+    
+    
     regex = re.compile(r'<span class="mr-2">'
            r'(\S*)'
            r'</span>')
-    t = readFile('source.txt')
-    #templist = findMatches(t, regex)
-    templist = [
-        '10.10.53.101', '10.10.53.2', 'google.com'
-    ]
-    print(generateProxy(templist))
-    
-        
+    '''
+    t = getContent('https://nordvpn.com/ru/ovpn/')
+    l = findMatches(t, regex)
+    with open ('all_servers.txt', 'w') as f:
+        for line in l:
+            f.write(line + '\r\n')    
+    '''    
+    t = getContent('https://nordvpn.com/ru/ovpn/')
+    l = findMatches(t, regex)
+    print(generateProxy(l))
     
     
